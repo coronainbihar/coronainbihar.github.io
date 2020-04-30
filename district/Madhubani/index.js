41a265ae486df2d641e68cbb7bce133dad851e32
coronainbihar <64576921+coronainbihar@users.noreply.github.com>
2020-04-30 12:42:39 +0530

    District files uploaded

---
+++ district/Madhubani/index.js
@@ -0,0 +1,26 @@
+var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+	  if (this.readyState == 4 && this.status == 200) {
+		
+		var myObj = JSON.parse(this.responseText);
+		var len = myObj.length ;
+
+		var htxt = "" ;
+		htxt += "<c>Confirmed<br>"+myObj[len-1].confirmed+"</c><a >Active<br>"+myObj[len-1].active+"</a><r>Recovered<br>"+myObj[len-1].recovered+"</r><d>Deceased<br>"+myObj[len-1].deceased+"</d>" ;
+		document.getElementById("hdemo").innerHTML = htxt;
+		
+		var txt = "" ;
+		txt += "<table>" ;
+
+		txt += "<tr><th>Block</th><th>New Confirmed</th><th>Confirmed</th><th>Active</th><th>Recovered</th><th>Deceased</th></tr>" ;
+		var x ;
+		for (x = 0; x < len-1; x++) {
+		  /* txt += "<tr><td>" + myObj[x].name + "</td></tr>"; */
+		  txt += "<tr><td>"+myObj[x].block+"</td><td>"+myObj[x].new_confirmed+"</td><td>"+myObj[x].confirmed+"</td><td>"+myObj[x].active+"</td><td>"+myObj[x].recovered+"</td><td>"+myObj[x].deceased+"</td></tr>" ;
+		}
+		txt += "</table>"    
+		document.getElementById("demo").innerHTML = txt;
+	  }
+	};
+	xmlhttp.open("GET", "Madhubanicsv.json", true);
+	xmlhttp.send();

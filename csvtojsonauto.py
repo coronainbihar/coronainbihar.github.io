4dec4b6d444c8f9ed218d72f0e9deb1ed14b9bc8
anandv01 <anandv01@users.noreply.github.com>
2020-04-30 13:27:39 +0530

    data updated

---
+++ csvtojsonauto.py
@@ -0,0 +1,50 @@
+import csv
+import json
+import os
+
+csvfile = open('bihartotal.csv', 'r')
+jsonfile = open('Biharcsv.json', 'w')
+
+reader1 = csv.reader(csvfile)
+header = tuple(next(reader1))  # gets the first line
+print header
+fieldnames = header
+reader = csv.DictReader( csvfile, fieldnames)
+
+jsonfile.write('[')
+check = True
+
+for row in reader:
+	if (row['block']=='Total'):
+		if (row['district']!='Bihar') :
+			json.dump(row, jsontemp, indent=4, sort_keys=False)
+			jsontemp.write('\n')
+			jsontemp.write(']')
+			json.dump(row, jsonfile, indent=4, sort_keys=False)
+			jsonfile.write(',')
+			jsonfile.write('\n')
+			check = True
+		else :
+			json.dump(row, jsonfile, indent=4, sort_keys=False)
+			jsonfile.write('\n')
+			jsonfile.write(']')
+	else :
+		if (check):
+			dis = row['district']
+			url = 'district/'+dis+'/'+dis+'csv.json'
+			
+			if not os.path.exists(os.path.dirname(url)):
+				try:
+					os.makedirs(os.path.dirname(url))
+				except OSError as exc: # Guard against race condition
+					if exc.errno != errno.EEXIST:
+						raise
+						
+			jsontemp = open(url, 'w')
+			jsontemp.write('[')
+			check = False
+		json.dump(row, jsontemp, indent=4, sort_keys=False)
+		jsontemp.write(',')
+		jsontemp.write('\n')
+		
+	

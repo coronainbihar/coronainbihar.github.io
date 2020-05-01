5b5c94742b8013b008790e39b2d0fee0ff2e131c
anandv01 <anandv01@users.noreply.github.com>
2020-05-01 13:53:24 +0530

    sorting update

--- district/Supaul/index.js
+++ district/Supaul/index.js
@@ -39,7 +39,7 @@ var xmlhttp = new XMLHttpRequest();
     rows = table.rows;
     /* Loop through all table rows (except the
     first, which contains table headers): */
-    for (i = 1; i < (rows.length - 2); i++) {
+    for (i = 1; i < (rows.length - 1); i++) {
       // Start by saying there should be no switching:
       shouldSwitch = false;
       /* Get the two elements you want to compare,

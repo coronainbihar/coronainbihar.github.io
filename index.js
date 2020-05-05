84a14661094ec23a7ef14bce4fe9f88f520a78bb
anandv01 <anandv01@users.noreply.github.com>
2020-05-05 19:19:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ xmlhttp.onreadystatechange = function() {
     var txt = "" ;
     txt += "<table><thead>" ;
 
-    txt += "<tr><th onclick=\"sortTable(0, 0)\">District &#8645</th><th onclick=\"sortTable(1, 1)\" style=\"color:red;\">New Confirmed &#8645</th><th onclick=\"sortTable(2, 1)\" style=\"color:orange;\">Confirmed &#8645</th><th onclick=\"sortTable(3, 1)\" style=\"color:blue;\">Active &#8645</th><th onclick=\"sortTable(4, 1)\" style=\"color:green;\">Recovered &#8645</th><th onclick=\"sortTable(5, 1)\" style=\"color:red;\">Deceased &#8645</th></tr></thead><tbody>" ;
+    txt += "<tr><th onclick=\"sortTable(0, 0)\">District &#8645</th><th onclick=\"sortTable(1, 1)\" style=\"color:red;\">New Confirmed</th><th onclick=\"sortTable(2, 1)\" style=\"color:orange;\">Confirmed &#8645</th><th onclick=\"sortTable(3, 1)\" style=\"color:blue;\">Active &#8645</th><th onclick=\"sortTable(4, 1)\" style=\"color:green;\">Recovered &#8645</th><th onclick=\"sortTable(5, 1)\" style=\"color:red;\">Deceased &#8645</th></tr></thead><tbody>" ;
     var x ;
     for (x = 0; x < len-1; x++) {
       /* txt += "<tr><td>" + myObj[x].name + "</td></tr>"; */
